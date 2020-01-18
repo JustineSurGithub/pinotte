4ae7d69bb06fc3c7cb6bedc37313004338686778
Olivia Gelinas <oliviagelinas1@gmail.com>
2020-01-18 17:37:00 -0500

    update stack

--- ng-three-template/src/app/pileDechet/pileDechet.component.ts
+++ ng-three-template/src/app/pileDechet/pileDechet.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DechetsService } from '../services/dechets.service';
 
 @Component({
   selector: 'app-pile-dechet',
@@ -7,15 +8,19 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 export class pileDechetComponent implements OnInit {
 
 
-  pile : Number[] = [];
+  pile: Number[] = [];
 
-  constructor() {
-    
+  constructor(private dechetService: DechetsService) {
+
   }
 
 
   ngOnInit() {
-    this.pile = [1, 2, 3, 4, 5, 6, 7]
+    this.pile = [1, 2, 3, 4, 5, 6, 7];
+
+    this.dechetService.dechetAddingTimer.subscribe(() => {
+      this.pile.push(this.pile[this.pile.length-1] + 1);
+    });
   }
 
 }
--- ng-three-template/src/app/services/dechets.service.ts
+++ ng-three-template/src/app/services/dechets.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DechetsService {
+
+  dechetAddingTimer = new BehaviorSubject(0);
+
   startDechetStackUpdates(): any {
     window.setInterval(() => {
-      console.log('HELLO');
+      this.dechetAddingTimer.next(0);
     }, 1000);
   }
 
