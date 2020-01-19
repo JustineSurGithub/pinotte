d2b287b71a77653f7bbd1fee64d64ec46b3a77c0
fabricech <fabrice.charbonneau@hotmail.com>
2020-01-18 23:26:31 -0500

    main qui bouge

--- ng-three-template/src/app/engine/engine.service.ts
+++ ng-three-template/src/app/engine/engine.service.ts
@@ -14,6 +14,8 @@ export class EngineService implements OnDestroy {
 
   private cube: THREE.Mesh;
 
+  private hand: THREE.Sprite;
+
   private frameId: number = null;
   private backgroundScene: THREE.Scene;
   private backgroundCamera: THREE.Camera;
@@ -82,8 +84,18 @@ export class EngineService implements OnDestroy {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-      this.cube = new THREE.Mesh( geometry, material );
-      this.scene.add(this.cube);
+    this.cube = new THREE.Mesh( geometry, material );
+    this.scene.add(this.cube);
+
+
+    //main
+    var spriteMap = new THREE.TextureLoader().load( "/assets/main.png" );
+    var spriteMaterial = new THREE.SpriteMaterial( { map: spriteMap, color: 0xffffff } );
+    this.hand = new THREE.Sprite( spriteMaterial );
+    this.hand.position.z = 3;
+    this.hand.position.y = 1;
+    this.scene.add( this.hand );
+      
   }
 
   animate(): void {
@@ -110,11 +122,11 @@ export class EngineService implements OnDestroy {
         }
         if (e.keyCode === 37) {
           //left
-          
+          this.hand.translateX(-0.3);
         }
         if (e.keyCode === 39) {
           //right
-          
+          this.hand.translateX(0.3);
         }
       };
     });
