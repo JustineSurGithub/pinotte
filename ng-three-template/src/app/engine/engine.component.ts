53a8c18cddf400e745b353110eb5deac31beb0e0
Olivia Gelinas <oliviagelinas1@gmail.com>
2020-01-18 21:39:17 -0500

    Activate background rendering of photo

--- ng-three-template/src/app/engine/engine.component.ts
+++ ng-three-template/src/app/engine/engine.component.ts
@@ -14,7 +14,7 @@ export class EngineComponent implements OnInit {
 
   ngOnInit() {
     this.engServ.createScene(this.rendererCanvas);
-    //this.engServ.background();
+    this.engServ.background();
     this.engServ.animate();
   }
 
